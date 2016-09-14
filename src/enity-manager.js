d6d16467e968f1fb178e0d17d23084fb3eb3f1be
Cody Wilhelm <Cody Wilhelm>
2016-09-14 15:23:09 -0500

    after class 9/14

---
+++ src/enity-manager.js
@@ -0,0 +1,25 @@
+module.exports = exports = enityManager;
+
+function EnityManager(width, height, cellSize){
+	this.worldWidth = width;
+	this.worldHeight;
+	this.cellSize = cellSize;
+	this.widthInCells = Math.ceiling(width / cellSize);
+	this.numberOfCells = this.widthInCells * Math.ceil(height/cellSize);
+	this.cells = [];
+	for(car i = 0; i < this.numberOfCells; i++){
+		this.cells[i] = [];
+	}
+}
+
+EnityManager.prototype.addEnity() = function(x, y, width, height){
+	var left = Math.floor(enity.x / this.cellSize);
+	var right = Math.ceil((entity.x + enity.eidth) / this.cellSize);
+	var top = Math.floor(enity.y / this.cellSize);
+	var bottom = math.ceil((entity.y + entity.height) / this.cellSize);
+	for(var x = left; x<right;x++){
+		for(var y = top; y<bottom;y++){
+			this.cells.push(entity);
+		}
+	}
+}
